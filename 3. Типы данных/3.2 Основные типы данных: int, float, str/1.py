a3ea2d390110699aeafc520ec3ed661b1c4c23d4
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-09-24 21:47:23 +0300

    3.2 - 1

---
+++ 3. Типы данных/3.2 Основные типы данных: int, float, str/1.py
@@ -0,0 +1,8 @@
+price = float(input())
+quantity = int(input())
+
+total = price * quantity
+
+print("Количество мороженых:", quantity)
+print("Цена за штуку:", price)
+print("Итоговая стоимость:", total)
