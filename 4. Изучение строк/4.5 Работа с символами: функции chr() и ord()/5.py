350a585a7a3717e3e32df9266b46dc79c5f62217
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-11-12 19:50:15 +0300

    4.5 - 5

---
+++ 4. Изучение строк/4.5 Работа с символами: функции chr() и ord()/5.py
@@ -0,0 +1,5 @@
+s = input()
+
+new_s = s.replace("a", str(ord("a"))).replace("e", str(ord("e"))).replace("i", str(ord("i"))).replace("o", str(ord("o"))).replace("u", str(ord("u"))).replace("y", str(ord("y"))).replace("A", str(ord("A"))).replace("E", str(ord("E"))).replace("I", str(ord("I"))).replace("O", str(ord("O"))).replace("U", str(ord("U"))).replace("Y", str(ord("Y")))
+
+print(new_s)
