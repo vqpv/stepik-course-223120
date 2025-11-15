fce06134786df2a7004af4370de8c692c8a4515e
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-11-15 12:24:50 +0300

    5.1 - 3

---
+++ 5. Условные операторы/5.1 Логика условий: использование if, elif, else/3.py
@@ -0,0 +1,14 @@
+mark = int(input())
+
+if mark == 5:
+    print("Отлично")
+elif mark == 4:
+    print("Хорошо")
+elif mark == 3:
+    print("Удовлетворительно")
+elif mark == 2:
+    print("Неудовлетворительно")
+elif mark == 1:
+    print("Очень плохо")
+else:
+    print("Ошибка: оценка должна быть от 1 до 5.")
