07ecf40e0b62f62b36ca314da8e030399ff77a18
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-11-17 20:50:40 +0300

    5.1 - 6

---
+++ 5. Условные операторы/5.1 Логика условий: использование if, elif, else/6.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+if n == 1:
+    print("Понедельник")
+elif n == 2:
+    print("Вторник")
+elif n == 3:
+    print("Среда")
+elif n == 4:
+    print("Четверг")
+elif n == 5:
+    print("Пятница")
+elif n == 6:
+    print("Суббота")
+elif n == 7:
+    print("Воскресенье")
+else:
+    print("Некорректный ввод")
