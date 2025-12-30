3f866302d99202970fcbb442329540b8e8d2df2d
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-12-30 22:13:53 +0300

    6.4 - 1

---
+++ 6. Изучаем циклы/6.4 Операторы break и continue/1.py
@@ -0,0 +1,9 @@
+word = input()
+
+while True:
+    if word == "сезам":
+        print("Волшебное слово произнесено!")
+        break
+    else:
+        print("Попробуйте ещё...")
+        word = input()
