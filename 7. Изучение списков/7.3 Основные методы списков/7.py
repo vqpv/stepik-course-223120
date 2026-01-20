3b414f128f4995c692027f5f480b248e832666fc
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-01-20 23:07:39 +0300

    7.3 - 7

---
+++ 7. Изучение списков/7.3 Основные методы списков/7.py
@@ -0,0 +1,9 @@
+planets = ['Меркурий', 'Венера', 'Земля', 'Марс', 'Юпитер', 'Сатурн', 'Нептун', 'Плутон']
+
+p_1 = input()
+p_2 = input()
+
+planets.append(p_1)
+planets.remove(p_2)
+
+print(planets)
